cc8a95ac3bb7e7b0e328412c9b53ccd499db1911
gclarkjr5 <gclarkjr5@gmail.com>
2022-05-23 12:53:08 -0500

    adding some basic comments

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ use f_to_c::temperature::convert;
 use f_to_c::temperature::convert::Temperature;
 
 fn main() -> Result<(), &'static str> {
+
+    // get the original temp type from the user (f or c)
     let from_temp_type = convert::read_input("What temperature do you want to convert from? Type 'c' or 'f'.");
     let tt = convert::parse_temp_type(from_temp_type)?;
 
+    // get the original temp value of the above temp type
     let from_temp_value = convert::read_input("What value temperature do you want to convert?");
     let tv = convert::parse_temp_value(from_temp_value)?;
 
+    // construct the original temp
     let from_temp = Temperature {
         temp_type: tt,
         temp_value: tv
     };
 
+    // convert
     let to_temp = Temperature::convert(&from_temp);
 
     println!("{} in {:?} is equal to {} in {:?}", from_temp.temp_value, from_temp.temp_type, to_temp.temp_value, to_temp.temp_type);
